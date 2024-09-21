955adee38a36f140d70a2a43974f8a7cd07f14ad
Alex Brodnicki <apbrodnicki@gmail.com>
2024-09-20 23:25:43 -0500

    Terrible but borderline passable move dialog styling (if you count just throwing all of the properties in).

--- src/components/lookup/move/MoveDialog.tsx
+++ src/components/lookup/move/MoveDialog.tsx
@@ -1,4 +1,4 @@
-import { Box, Dialog, DialogTitle, List, ListItem, Typography } from '@mui/material';
+import { Box, Dialog, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 import { defaultMove } from 'data';
 import type { Move } from 'models/models';
 import React from 'react';
@@ -20,89 +20,108 @@ export const MoveDialog = (
 	};
 
 	return (
-		<Dialog open={isMoveDialogOpen} onClose={onClose} sx={{ p: 3 }}>
+		<Dialog
+			open={isMoveDialogOpen}
+			onClose={onClose}
+			sx={{
+				p: 3,
+				'& .MuiDialog-paper': {
+					backgroundColor: '#B8D8D8'
+				}
+			}}
+		>
 			<DialogTitle>
-				<Typography>
-					{move.name}
-				</Typography>
+				{move.name}
 				<Box
 					component='img'
 					src={`src/assets/types/${move.type}.png`}
 					alt='type'
 				/>
 			</DialogTitle>
-			<List>
-				<ListItem>
-					<Typography>
-						{move.description}
-					</Typography>
-				</ListItem>
-				<ListItem>
-					<Typography>
-						accuracy: {move.accuracy}
-					</Typography>
-				</ListItem>
-				<ListItem>
-					<Typography>
-						ailment: {move.ailment}
-					</Typography>
-				</ListItem>
-				<ListItem>
-					<Typography>
-						class: {move.damageClass}
-					</Typography>
-					<Box
-						component='img'
-						src={`src/assets/damage-class/${move.damageClass}.png`}
-						alt='type'
-					/>
-				</ListItem>
-				<ListItem>
-					<Typography>
-						effectChance: {move.effectChance}
-					</Typography>
-				</ListItem>
-				<ListItem>
-					<Typography>
-						category: {move.category}
-					</Typography>
-				</ListItem>
-				<ListItem>
-					<Typography>
-						crit rate: {move.critRate}
-					</Typography>
-				</ListItem>
-				<ListItem>
-					<Typography>
-						isContact: {move.isContact ? 'true' : 'false'}
-					</Typography>
-				</ListItem>
-				<ListItem>
-					<Typography>
-						crit rate: {move.critRate}
-					</Typography>
-				</ListItem>
-				<ListItem>
-					<Typography>
-						crit rate: {move.critRate}
-					</Typography>
-				</ListItem>
-				<ListItem>
-					<Typography>
-						crit rate: {move.critRate}
-					</Typography>
-				</ListItem>
-				<ListItem>
-					<Typography>
-						crit rate: {move.critRate}
-					</Typography>
-				</ListItem>
-				<ListItem>
-					<Typography>
-						crit rate: {move.critRate}
-					</Typography>
-				</ListItem>
-			</List>
+			<DialogContent>
+				<DialogContent>
+					<DialogContentText>
+						{move.damageClass}
+						<Box
+							component='img'
+							src={`src/assets/damage-class/${move.damageClass}.png`}
+							alt='type'
+						/>
+					</DialogContentText>
+					<DialogContentText>
+					power: {move.power}
+					</DialogContentText>
+					<DialogContentText>
+					accuracy: {move.accuracy}
+					</DialogContentText>
+				</DialogContent>
+				<DialogContent>
+					<DialogContentText>
+					description: {move.description}
+					</DialogContentText>
+				</DialogContent>
+				<DialogContent>
+					<DialogContentText>
+					ailment: {move.ailment}
+					</DialogContentText>
+					<DialogContentText>
+					effect chance: {move.effectChance}
+					</DialogContentText>
+					<DialogContentText>
+					category: {move.category}
+					</DialogContentText>
+					<DialogContentText>
+					crit rate: {move.critRate}
+					</DialogContentText>
+					<DialogContentText>
+					flinch chance: {move.flinchChance}
+					</DialogContentText>
+					<DialogContentText>
+					is contact: {move.isContact ? 'yes' : 'no'}
+					</DialogContentText>
+					<DialogContentText>
+					target: {move.target}
+					</DialogContentText>
+					<DialogContentText>
+					priority: {move.priority}
+					</DialogContentText>
+					<DialogContentText>
+					pp: {move.powerPoints}
+					</DialogContentText>
+				</DialogContent>
+				<DialogContent>
+					{move.statChanges.map((statChange, index) => (
+						<DialogContentText key={index}>
+						stat change | {statChange.stat}: {statChange.change}
+						</DialogContentText>
+					))}
+					<DialogContentText>
+					stat change chance: {move.statChangeChance}
+					</DialogContentText>
+				</DialogContent>
+				<DialogContent>
+					<DialogContentText>
+					drain: {move.drain}
+					</DialogContentText>
+					<DialogContentText>
+					healing: {move.healing}
+					</DialogContentText>
+				</DialogContent>
+				<DialogContent>
+					<DialogContentText>
+					max hits: {move.maxHits}
+					</DialogContentText>
+					<DialogContentText>
+					min hits: {move.minHits}
+					</DialogContentText>
+					<DialogContentText>
+					max turns: {move.maxTurns}
+					</DialogContentText>
+					<DialogContentText>
+					min turns: {move.minTurns}
+					</DialogContentText>
+				</DialogContent>
+			</DialogContent>
 		</Dialog>
 	);
 };

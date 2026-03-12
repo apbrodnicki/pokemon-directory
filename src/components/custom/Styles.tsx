9d95a8a4c90ad2a2996274b9ad92568689f88087
Alex Brodnicki <apbrodnicki@gmail.com>
2026-03-12 02:31:45 -0500

    Forgot a file

---
+++ src/components/custom/Styles.tsx
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+import { ListItem, Paper } from '@mui/material';
+
+export const StyledOptionListItem = styled(ListItem)(() => ({
+	backgroundColor: '#B8D8D8',
+	'&.MuiAutocomplete-option.Mui-focused': {
+		backgroundColor: '#DDEDED'
+	},
+}));
+
+export const StyledGroupListItem = styled(ListItem)(() => ({
+	padding: 0,
+	flexDirection: 'column' as const,
+	alignItems: 'flex-start'
+}));
+
+export const StyledPaper = styled(Paper)(() => ({
+	margin: '24px',
+	backgroundColor: '#B8D8D8',
+	width: '33%'
+}));
